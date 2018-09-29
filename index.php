c6552f71d9d65fc0509ae1549249d2b48caed99e
Alfonso <alfonso.cisternas3@gmail.com>
2018-09-29 17:32:27 -0300

    My first commit!

---
+++ index.php
@@ -0,0 +1,196 @@
+<!doctype html>
+<html lang="Default">
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="./css/alert.css">
+	
+	<link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/linecons.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lobster&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    
+	<script src="./js/jquery-2.1.0.min.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
+	<script src="./js/blocs.min.js"></script>
+	<script src="./js/lazysizes.min.js" defer></script>
+	
+    <title>Home</title>
+<script>
+$ (window).ready (function () {
+	setTimeout (function () {
+		$ ('#modal-subscribe').modal ("show")
+	}, 300)
+})
+</script>
+    
+<!-- Google Analytics -->
+ 
+<!-- Google Analytics END -->
+    
+</head>
+<body>
+<!-- Main container -->
+<div class="page-container">
+    
+<!-- bloc-0 -->
+<div class="bloc bgc-white l-bloc" id="bloc-0">
+	<div class="container bloc-sm">
+		<nav class="navbar row">
+			<div class="navbar-header">
+				<a class="navbar-brand " href="index.php"><img src="img/logo.jpg" alt="logo" width="173" />Autopartes Premiun</a>
+				<button id="nav-toggle" type="button" class="ui-navbar-toggle navbar-toggle" data-toggle="collapse" data-target=".navbar-1">
+					<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</button>
+				<h3 class="mg-md text-right ">
+					La experiencia de comprar repuestos
+				</h3>
+			</div>
+			<div class="collapse navbar-collapse navbar-1 special-dropdown-nav">
+				<ul class="site-navigation nav navbar-nav">
+					<li>
+						<a href="home.html">Home</a>
+					</li>
+					<li>
+						<a href="contacto.html">Contacto</a>
+					</li>
+					<li>
+						<a href="index.php">Cotiza con nosotros</a>
+					</li>
+					<li>
+						<a href="index.php">Compra aquí</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</div>
+<!-- bloc-0 END -->
+
+<!-- bloc-1 -->
+<div class="bloc bgc-charcoal b-parallax tc-white d-bloc" id="bloc-1">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-xs-12 col-md-8 col-md-offset-2">
+				<h2 class="mg-md text-center tc-medium-jungle-green">
+					Quienes somos
+				</h2>
+				<h3 class="statement-bloc-text text-center tc-medium-jungle-green ">
+					Somos una empresa especializada en importación y venta de repuestos para vehículos Toyota y Lexus. Manejamos un amplio stock de repuestos Genuinos y alternativos de las mejores marcas los cuales entregamos y despachamos a todo Chile. <br><br>Disfrute de una atención personalizada, seria, responsable y comprometida. <br><br>Además para su comodidad y para entregar un mejor servicio a nuestros clientes usted puede contactarnos por diversos medios.<br>
+				</h3>
+				<p class="text-center">
+					<strong>Marcelo Silva</strong>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-1 END -->
+
+<!-- bloc-2 -->
+<div class="bloc bgc-charcoal b-parallax d-bloc" id="bloc-2">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1 class="mg-md tc-white">
+					Nuestro Productos
+				</h1>
+			</div>
+		</div>
+		<div class="row voffset">
+			<div class="col-sm-4">
+				<h2 class="mg-md tc-white">
+					Filtros
+				</h2><img src="img/lazyload-ph.png" data-src="img/placeholder-image.png" class="img-responsive lazyload" />
+				<p class=" mg-sm">
+					Filtro de aire: Retiene las posibles impurezas que puedan acceder al circuito de admisión del motor, evitando la contaminación de la cámara de combustión.<br><br>Filtros de aceite: Protege el aceite lubricante de las impurezas que puedan llegar a él antes de que inicie su recorrido de lubricación por el circuito de engrase del motor.<br><br>Filtro de cabina/polen: Evita que se alojen dentro del automóvil, sobre todo durante la época primaveral, las moléculas de polen que tan perjudiciales resultan para personas alérgicas y/o asmáticas<br>
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<h2 class="mg-md tc-white">
+					Frenos
+				</h2><img src="img/lazyload-ph.png" data-src="img/placeholder-image.png" class="img-responsive lazyload" />
+				<p class=" mg-sm">
+					Las pastillas de freno proporcionan la fricción necesaria a los discos de freno de tal forma que cuando accionamos el pedal, nuestro vehículo se detiene. <br><br> <br>Disponemos sólo de productos originales Toyota/Lexus o de marcas especializadas con desempeños de alto estándar. Su seguridad en este tipo de productos está en juego.<br>
+				</p>
+			</div>
+			<div class="col-sm-4">
+				<h2 class="mg-md tc-white">
+					Bujías
+				</h2><img src="img/lazyload-ph.png" data-src="img/placeholder-image.png" class="img-responsive lazyload" />
+				<p class=" mg-sm">
+					La bujía es el elemento que produce el encendido de la mezcla de combustible y oxígeno en los cilindros, mediante una chispa, en un motor de combustión interna de encendido provocado. <br><br>Disponemos de un amplio Stock de productos Denso que es la marca propia parte del Holding Toyota y procurando siempre el uso de la bujía recomendada por el fabricante.<br>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-2 END -->
+
+<!-- ScrollToTop Button -->
+<a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span class="fa fa-chevron-up"></span></a>
+<!-- ScrollToTop Button END-->
+
+
+<!-- Footer - bloc-5 -->
+<div class="bloc bgc-white l-bloc" id="bloc-5">
+	<div class="container bloc-md">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3 class="mg-md bloc-mob-center-text text-left">
+					Confiables
+				</h3><a class="a-btn a-block bloc-mob-center-text pull-left-block" onclick="scrollToTarget('1')" target="_blank">Inicio</a><a href="contacto.html" class="a-btn a-block bloc-mob-center-text pull-left-block">Contactanos</a>
+			</div>
+			<div class="col-sm-4">
+				<h3 class="mg-md bloc-mob-center-text text-left">
+					Nuestras Marcas
+				</h3><a href="http://www.toyota.com" class="a-btn a-block bloc-mob-center-text pull-left-block">Toyota</a><a href="http://www.lexus.com" class="a-btn a-block bloc-mob-center-text pull-left-block">Lexus</a>
+			</div>
+			<div class="col-sm-4">
+				<h3 class="mg-md bloc-mob-center-text text-left">
+					Siguenos
+				</h3><a href="https://www.facebook.com/AutoPartes-PremiumRepuestos-Toyota-y-Lexus-1580093928904206/?ref=br_rs" class="a-btn a-block bloc-mob-center-text pull-left-block" target="_blank">Facebook</a><a href="index.php" class="a-btn a-block bloc-mob-center-text pull-left-block">Red Social</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Footer - bloc-5 END -->
+
+</div>
+<!-- Main container END -->
+    
+<div class="modal fade" id="modal-subscribe" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header modal-bg border-0">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<h4 class="modal-title" id="myModalLabel"></h4>
+</div>
+<div class="modal-body modal-bg">
+<p class="text-center h1-modal">This is an alert box!</p>
+<div class="row">
+<div class="col-sm-10 col-sm-offset-1">
+<p class="text-center h2-modal">Add your HTML content and style it as you like.</p>
+<p class="text-muted small text-center modal-text">Here's some modal-text and an example link<br/><a href="https://duckduckgo.com" class="link-mod">Search with DuckDuckGo</a>.</p>
+<div class="col-sm-10 col-sm-offset-1">
+<br/>
+<p class="text-center">
+<button type="button" class="btn btn-enter" data-dismiss="modal">Enter site</button></p></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!-- Preloader -->
+<div id="page-loading-blocs-notifaction"></div>
+<!-- Preloader END -->
+
+</body>
+</html>
